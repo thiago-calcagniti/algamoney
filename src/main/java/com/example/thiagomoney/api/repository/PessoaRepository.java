027c54fa1e5b7bcf2c1250b77c2526de52d49910
tcalcagniti <tiako_@hotmail.com>
2021-04-04 14:39:44 -0300

    adicionado pesquisa de pessoas pelo nome

--- src/main/java/com/example/thiagomoney/api/repository/PessoaRepository.java
+++ src/main/java/com/example/thiagomoney/api/repository/PessoaRepository.java
@@ -1,5 +1,7 @@
 package com.example.thiagomoney.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.thiagomoney.api.model.Pessoa;
@@ -9,4 +11,7 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 	public default Pessoa find(Long codigo) {
 		return this.findById(codigo).orElse(null);
 	}
+	
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
+	
 }
